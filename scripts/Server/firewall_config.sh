c6ebffad8501865b778728a9950cc72995f330b2
cupkes <chris_deColorado@yahoo.com>
2016-05-19 12:53:40 -0700

    fixed if error, added reload
    added the fi to the 3rd if statement, and added the firewall-cmd
    --reload statement.

--- scripts/Server/firewall_config.sh
+++ scripts/Server/firewall_config.sh
@@ -12,54 +12,57 @@ logger -p local0.notice -t $LOGTAG "modifying iptables"
 
 # open http and https ports
 firewall-cmd --permanent --add-port=7474/tcp
-if [ $? -eq 0 ]; then
+
+if [ $? -ne 0 ]; then
 		echo "firewall operation error"
 		logger -p local0.notice -t $LOGTAG "error modifying iptables"
 		exit 1
 fi
 firewall-cmd --permanent --add-port=7473/tcp
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
 		echo "firewall operation error"
 		logger -p local0.notice -t $LOGTAG "error modifying iptables"
 		exit 2
+fi
 # open cluster management ports
 firewall-cmd --permanent --add-port=5001/tcp
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
 		echo "firewall operation error"
 		logger -p local0.notice -t $LOGTAG "error modifying iptables"
 		exit 3
 fi
 firewall-cmd --permanent --add-port=6001/tcp
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
 		echo "firewall operation error"
 		logger -p local0.notice -t $LOGTAG "error modifying iptables"
 		exit 4
 fi
 # open Samba ports
 firewall-cmd --permanent --add-port=137/udp
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
 		echo "firewall operation error"
 		logger -p local0.notice -t $LOGTAG "error modifying iptables"
 		exit 5
 fi
 firewall-cmd --permanent --add-port=138/udp
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
 		echo "firewall operation error"
 		logger -p local0.notice -t $LOGTAG "error modifying iptables"
 		exit 6
 fi
 firewall-cmd --permanent --add-port=139/tcp
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
 		echo "firewall operation error"
 		logger -p local0.notice -t $LOGTAG "error modifying iptables"
 		exit 7
 fi
 firewall-cmd --permanent --add-port=445/tcp
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
 		echo "firewall operation error"
 		logger -p local0.notice -t $LOGTAG "error modifying iptables"
 		exit 8
 fi
+firewall-cmd --reload
 echo "ports opened: "
 firewall-cmd --list-ports
 
