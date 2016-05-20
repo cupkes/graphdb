bed78d0c0a8a43cc42a5a5efccbf1ba1e0aeb180
cupkes <chris_deColorado@yahoo.com>
2016-05-20 14:33:12 -0700

    added more files, updated server scripts
    Added more unit test and support files.  Updated server files

--- scripts/Server/test_neo4j_server_init.sh
+++ scripts/Server/test_neo4j_server_init.sh
@@ -46,12 +46,12 @@ ADMIN_EMAIL=dongyang@microsoft.com
 # provide the crontab entry for the intrusion detection module
 AIDE_ENTRY="0 1 * * * /usr/sbin/aide --check"
 # detail the hostnames and ip addresses of all nodes in the cluster
-NODE1_HN=3DM-Graph01
-NODE2_HN=3DM-Graph02
-NODE3_HN=3DM-Graph03
-NODE1_IP=10.1.0.7
-NODE2_IP=10.1.0.6
-NODE3_IP=10.1.0.10
+NODE1_HN=3DM-Graph04
+NODE2_HN=3DM-Graph05
+NODE3_HN=3DM-Graph06
+NODE1_IP=10.1.0.11
+NODE2_IP=10.1.0.12
+NODE3_IP=10.1.0.13
 # the node details of the script destination
 THISH=$NODE1_HN
 THISIP=$NODE1_IP
@@ -72,7 +72,7 @@ mkdir -p $NEOBIN && chmod 755 $NEOBIN
 mkdir -p $NEOBAK && chmod 755 $NEOBAK
 #
 logger -p local0.notice -t $LOGTAG "neo4j home and support directories created"
-
+echo "creating directories complete"
 #------------------------------------------------
 # Update current user (Neo4j) .bash_profile
 #------------------------------------------------
@@ -88,6 +88,8 @@ fi
 # update the profile script to include essential path values
 # and helpful aliases and shell configurations
 #
+echo "updating bash_profile script"
+
 cp ~/.bash_profile ~/.bash_profile.bak
 
 cat << ENDOC >> ~/.bash_profile
@@ -120,6 +122,8 @@ ENDOC
 
 logger -p local0.notice -t $LOGTAG "user $USER profile updated"
 
+echo "updating .bash_profile complete"
+
 #------------------------------------------------
 # Populate Neo4j directories with Neo4j files
 #------------------------------------------------
@@ -139,9 +143,13 @@ source .bash_profile
 
 # uncompress the neo4j support tarball
 
+echo "opening support tarball"
+
 tar -zxvf $SUPPORT_TGZ_FILE -C $NEOBASE && logger -p local0.notice -t $LOGTAG "support files loaded"
 
 # copying support files to proper directories
+
+echo "copying files to folders"
 
 cp $CONF_FILE $NEOETC/$CONF_FILE
 cp $DIAG_FILE $NEOBIN/$DIAG_FILE
@@ -153,12 +161,17 @@ cp $MONITOR_SCRIPT $NEOETC/$MONITOR_SCRIPT
 
 logger -p local0.notice -t $LOGTAG "support files deployed"
 
+echo "files copied"
+
 # uncompress neo4j enterprise server tarball
+
+echo "opening neo4j enterprise tarball"
 
 tar -zxvf $NEO4J_SERVER_TGZ $NEOBASE && logger -p local0.notice -t $LOGTAG "Neo4j Enterprise Server files deployed"
 
 # backup existing configuration files and copy over provided configuration files
 
+echo "copying config files to NEOHOME/conf"
 if [ -d $NEOCONF ]; then
 	cd $NEOCONF
 	mv $NEO4J_PROP_FILE $NEO4J_PROP_FILE.bak && cp $NEOETC/$NEO4J_PROP_FILE $NEO4J_PROP_FILE
@@ -181,62 +194,10 @@ else
 	echo "cannot locate neo4j home directory"
 	echo "manual configuration of neo4j cluster is required"
 	
-	logger -p local0.notice -t $LOGTAG "unable to deploy neo4j ccluster configuration files"
+	logger -p local0.notice -t $LOGTAG "unable to deploy neo4j cluster configuration files"
 fi
 
-#------------------------------------------------
-# Install intrusion detection package
-#------------------------------------------------
-
-# check to see if the aide package is already installed
-
-AIDETEST=$(yum list installed aide |& grep Error | awk '{ print $1 }' | sed s/://) 
-
-# install the aide package if it is missing
-
-if [ $AIDETEST == "Error" ]
-then
-    echo "AIDE not installed, installing" && logger -p local0.notice -t $LOGTAG "installing AIDE"
-	yum install -y aide
-else
-    echo "AIDE already installed"
-fi
-
-# update the crontab mailto configuration with the admin email
-
-sed -i 's/root/$ADMIN_EMAIL/' /etc/crontab
-
-# initialize the aide package and get the service started
-
-aide --init
-
-logger -p local0.notice -t $LOGTAG "AIDE initialized"
-
-# navigate to the aide library directory
-# the aide service creates a new database
-# so we rename the new database to the default
-
-cd /var/lib/aide
-mv aide.db.new.gz aide.db.gz
-
-# according to the manual, we need to invoke the check & update routines
-# and then switch to the newly created database
-
-aide --check
-aide --update
-
-rm aide.db.gz
-
-mv aide.db.new.gz aide.db.gz
-
-# now we update the crontab with an entry for the aide package
-
-crontab -e
-
-cat $AIDE_ENTRY >> /etc/crontab
-
-logger -p local0.notice -t $LOGTAG "crontab updated with AIDE entry"
-
+echo "config file copy complete"
 
 #------------------------------------------------
 # Set Neo4j Server specific os configurations
@@ -246,6 +207,8 @@ logger -p local0.notice -t $LOGTAG "crontab updated with AIDE entry"
 # we need to update some server configuration files
 # in order for the neo4j cluster to function properly
 #
+
+echo "modifying limits and pam.d/su files"
 
 cat << ENDOC >> /etc/security/limits.conf
 # NEO4J SUPPORT MODIFICATION
@@ -264,6 +227,7 @@ ENDOC
 
 logger -p local0.notice -t $LOGTAG "pam.d/su modified"
 
+echo "modification of limits.conf and pam.d/su complete"
 # A restart is required for the settings to take effect.
 # After the above procedure, the neo4j user will have a limit of 40 000 simultaneous open files.
 # If you continue experiencing exceptions on Too many open files or Could not stat() directory,
@@ -284,6 +248,7 @@ echo "System must be restarted before changes take affect"
 # We should improve this script by checking for multiple releases (6,7,8)
 # and multiple vendors (openjdk, Oracle).
 
+echo "installing jdk1.8.0"
 JDKTEST=$(yum list installed java-1.8.0-openjdk |& grep Error | awk '{ print $1 }' | sed s/://) 
 
 # if there is no jdk installed, install the openjdk 1.8.0 package
@@ -296,6 +261,8 @@ else
     echo "JDK already installed"
 fi
 
+echo "jdk install complete"
+
 #------------------------------------------------
 # Update network information
 #------------------------------------------------
@@ -305,6 +272,7 @@ fi
 # we add nodes to host file so we can reference
 # all nodes in the cluster by their actual hostnames
 #
+echo "updating hosts file"
 
 cat << ENDOC >> /etc/hosts
 # NEO4J SUPPORT MODIFICATION
@@ -316,142 +284,8 @@ ENDOC
 
 logger -p local0.notice -t $LOGTAG "/etc/hosts modified"
 
-#------------------------------------------------
-# Install and configure auditing
-#------------------------------------------------
 
-#
-# To ensure we meet the standard security certification guidelines,
-# we need to make sure auditing is installed and running rules
-# based on the considered requirements (stig, capp, nipsom, etc...).
-# 
-
-# check to see if auditing is installed and install if it is missing
-
-AUDITTEST=$(yum list installed audit |& grep Error | awk '{ print $1 }' | sed s/://) 
-
-if [ $AUDITTEST == "Error" ]
-then
-    echo "AUDIT not installed, installing" && logger -p local0.notice -t $LOGTAG "installing AUDIT"
-	yum install -y audit
-else
-    echo "audit already installed"
-fi
-
-# set audit rules -- here I use stig because it's thorough
-
-
-
-auditctl -R /usr/share/doc/audit-version/stig.rules
-if [ $? -eq 0 ]; then
-	echo "audit rules loaded"
-	logger -p local0.notice -t $LOGTAG "auditing rules loaded and auditing started"
-else
-	echo "unable to reload audit daemon with new rules"
-	echo "please execute auditctl -R and provide valid rules file"
-	logger -p local0.notice -t $LOGTAG "erro loading audit rules"
-fi
-
-
-# optionally you can copy the specific rules file to the default:
-#
-# cp /etc/audit/audit.rules /etc/audit/audit.rules_backup
-# cp /usr/share/doc/audit-version/stig.rules /etc/audit/audit.rules
-#
-# or nipsom.rules
-# or capp.rules
-# or lspp.rules
-
-#------------------------------------------------
-# Setup and configure samba
-#------------------------------------------------
-
-#
-# Next we need to setup samba so we can load files from windows
-# without the need for ssh or ftp.
-#
-
-# call samba config script
-
-cd $NEOBIN
-chmod +x $SAMBA_SCRIPT
-./$SAMBA_SCRIPT
-RETVAL=$?
-if [ $RETVAL -ne 0 ]; then
-	echo "samba script failed at exit $RETVAL"
-	exit 1
-else
-	echo "samba script successful"
-fi
-
-
-#------------------------------------------------
-# Configure IPTABLES
-#------------------------------------------------
-
-#
-# Although the Azure virtual switches control most of our network security
-# we want to ensure that no local firewall rules prevent
-# our neo4j cluser or other services (i.e. samba) from operating normally
-#
-
-# call the firewall configuration script
-
-cd $NEOBIN
-chmod +x $FIREWALL_SCRIPT
-./FIREWALL_SCRIPT
-RETVAL=$?
-if [ $RETVAL -ne 0 ]; then
-	echo "firewall script failed at exit $RETVAL"
-	exit 1
-else
-	echo "firewall script successful"
-fi
-
-
-#------------------------------------------------
-# Configure backup scheduler
-#------------------------------------------------
-
-#
-# we have a neo4j-specific backup script that defines
-# a weekly backup with incremental backups between weekly fulls.
-# Since the cluser will be batch-loaded, this backup script
-# will be copied to the neo4j_support/bin directory
-# but will not be copied to /etc/cron.daily
-#
-
-cp $BACKUP_SCRIPT $NEOBIN/$BACKUP_SCRIPT
-echo "backup script copied to $NEOBIN"
-echo "to schedule backup routine copy  $BACKUP_SCRIPT to /etc/cron.daily"
-
-#------------------------------------------------
-# Deploy monitoring script
-#------------------------------------------------
-
-#
-# A neo4j monitoring script already has a routine for monitoring
-# the space available in the neo4j/backup directory
-# Other monitoring routines should be added to the script.
-# 
-
-cp $MONITOR_SCRIPT /etc/cron.daily/$MONITOR_SCRIPT && chmod +x /etc/cron.daily/$MONITOR_SCRIPT
-
-logger -p local0.notice -t $LOGTAG "file : $MONITOR_SCRIPT added to cron.daily"
-
-
-#------------------------------------------------
-# Deploy and execute diagnostics script
-#------------------------------------------------
-
-#
-# The support diagnostics script will be copied to the neo4j_support/bin directory.
-# This script is used to provide the support team with a full set
-# of diagnostic information for use with troubleshooting production issues.
-#
-
-cp $SUPPORT_SCRIPT $NEOBIN/$SUPPORT_SCRIPT && chmod +x $NEOBIN/$SUPPORT_SCRIPT
-
+echo "hosts file update complete"
 #------------------------------------------------
 # Ensure proper ownership of directories and files
 #------------------------------------------------
@@ -461,12 +295,14 @@ cp $SUPPORT_SCRIPT $NEOBIN/$SUPPORT_SCRIPT && chmod +x $NEOBIN/$SUPPORT_SCRIPT
 # we want to make sure that we have the appropriate
 # user and group ownership defined
 #
+echo "changing file and folder ownnership"
+
 chown -R neo4j:neo4j $NEOBASE
 chown -R neo4j:neo4j $NEOSUPP
 
 logger -p local0.notice -t $LOGTAG "neo4j file and directory ownership changed"
 
-
+echo "test script complete"
 
 
 
